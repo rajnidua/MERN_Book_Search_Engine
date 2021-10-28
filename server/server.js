cdc9cb56d2573f3735462792a2ef803a4721b695
Rajni Dua <rajni.dua14@gmail.com>
2021-10-28 12:34:54 +1030

    created new instance of apollo server and pass it the imported schema

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
