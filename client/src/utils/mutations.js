84050797e19be783f95c60af76c1fa68c474b0ca
Rajni Dua <rajni.dua14@gmail.com>
2021-10-31 20:08:31 +1030

    user sign up added front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,6 +12,19 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        username
+        _id
+        email
+      }
+    }
+  }
+`;
+
 /* export const SAVE_BOOK = gql`
   mutation saveBook($book: BookInput!) {
     saveBook(book: $book) {
