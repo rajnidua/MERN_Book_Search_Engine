5960a076c61f361260d5f0aa47b9a27092f162c4
Rajni Dua <rajni.dua14@gmail.com>
2021-11-02 19:46:12 +1030

    added gitignore to server

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
-//const bcrypt = require("bcrypt");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
+//const bcrypt = require("bcryptjs");
 
 // import schema from Book.js
 const bookSchema = require("./Book");
